b714c07e97e957802bbf4bbeffb9c8dea6b3f0a2
shuvam dutta <sdutta@cloudbees.com>
2024-09-08 14:23:46 +0530

    syntax correction

--- git_commit.sh
+++ git_commit.sh
@@ -5,12 +5,12 @@ while [ $b != 0 ]
 do
 
   git checkout -b perf-test-$b
-  parallel=$b+4
+  parallel=$(($b+4))
   awk -v p="$parallel" 'BEGIN {FS="]"; OFS="";} NR==1 {$1=$1", "p"]"} {print $0}' JenkinsFile | tee  JenkinsFile
   git add *
   git commit -m "new branch commit  with increased parallel jobs"
   git push --set-upstream origin perf-test-$b
-  b=$b+1
+  b=$(($b+1))
 # commit frequency is calculated based on the time interval i.e sleep
   sleep 30
 
