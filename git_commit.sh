477f69e434cd2ee408bba348f9b53a6302a454da
shuvam dutta <sdutta@cloudbees.com>
2024-09-08 12:24:20 +0530

    added new branched creation in origin post commit to initiate triggers to build in controller

--- git_commit.sh
+++ git_commit.sh
@@ -9,6 +9,7 @@ do
   awk -v p=$parallel 'BEGIN {FS="]"; OFS=""} NR==1 {$1=$1", $p]"} {print $0}' JenkinsFile > JenkinsFile
   git add *
   git commit -m "new branch commit  with increased parallel jobs"
+  git push --set-upstream origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e sleep
   sleep 30
 
