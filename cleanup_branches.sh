cda5510a2a99dd67521f2456a757f68cc24caa84
shuvam dutta <sdutta@cloudbees.com>
2024-10-16 18:33:08 +0530

    syntax fixed in cleanup scripts

--- cleanup_branches.sh
+++ cleanup_branches.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-git branch | grep "^perf-" | xargs git push origin -d
-read -p "verify remote branches are cleared to proceed[Y] clearing local otherwise abort[N]: " flag
-
-if [ "$flag" -eq "Y"]; then 
-	git branch | grep "^perf-" | xargs git branch -D
-fi
+git branch | grep "^\s\+perf-test-" | xargs git push origin -d
+git branch | grep "^\s\+perf-test-" | xargs git branch -D
