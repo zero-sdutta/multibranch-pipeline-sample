778cfef08122b5e5c106d4bb38854dd75a0d6e9a
shuvam dutta <sdutta@cloudbees.com>
2024-10-16 18:40:59 +0530

    keeping user input check to proceed with local deletion to keep control

--- cleanup_branches.sh
+++ cleanup_branches.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 git branch | grep "^\s\+perf-test-" | xargs git push origin -d
-git branch | grep "^\s\+perf-test-" | xargs git branch -D
+
+read -p "Verify remote branches got deleted, proceed with local deletion? [0/1]: " flag
+
+if [ $flag -eq 1 ]
+then
+	git branch | grep "^\s\+perf-test-" | xargs git branch -D
+fi
