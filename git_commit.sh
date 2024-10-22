8a0e66eae9007519b963bc1b1f8215188cd62ea6
shuvam dutta <sdutta@cloudbees.com>
2024-10-22 13:16:32 +0530

    changed commit frequency to 5

--- git_commit.sh
+++ git_commit.sh
@@ -12,8 +12,9 @@ do
   git commit -m "new branch commit with incremental parallel jobs"
   git push --set-upstream origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
-  sleep 5
+  sleep 12
   echo $b >> ~/JaaS/iteration-output.txt
   b=$(($b+1))
 
 done
+date
