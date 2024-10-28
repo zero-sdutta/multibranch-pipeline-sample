cafe0f2809eece72362efab40b82802026e0820d
shuvam dutta <sdutta@cloudbees.com>
2024-10-28 13:01:09 +0530

    updated iteration output filename

--- git_commit.sh
+++ git_commit.sh
@@ -15,7 +15,7 @@ do
   git push origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
   sleep 15
-  echo $b >> ~/JaaS/iteration-output2.txt
+  echo $b >> ~/JaaS/iteration-output3.txt
   b=$(($b+1))
 
 done
