170bac2f23224e966bf0f655c452ba2dc00fa54e
shuvam dutta <sdutta@cloudbees.com>
2024-11-08 13:27:08 +0530

    caching commit password before run

--- git_commit.sh
+++ git_commit.sh
@@ -15,7 +15,7 @@ do
   git push origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
   sleep 6
-  echo $b >> ~/JaaS/iteration-output1.txt
+  echo $b >> ~/JaaS/iteration-output.txt
   b=$(($b+1))
 
 done
