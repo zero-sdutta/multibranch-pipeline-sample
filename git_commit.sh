115061a44e60ee8684150f24fc9ca342f34f3ea6
shuvam dutta <sdutta@cloudbees.com>
2024-11-11 14:17:43 +0530

    caching creds to start run

--- git_commit.sh
+++ git_commit.sh
@@ -31,7 +31,7 @@ do
   git push origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
   sleep 30
-  echo $b >> ~/JaaS/iteration-output-2cf.txt
+  echo $b >> ~/JaaS/iteration-output-2cf-test.txt
   b=$(($b+1))
 
 done
