f4c80e97734e262c3e2f9ab928b4ec3cc5d62f1c
shuvam dutta <sdutta@cloudbees.com>
2025-01-17 00:38:13 +0530

    cache creds

--- git_commit.sh
+++ git_commit.sh
@@ -31,7 +31,7 @@ do
   git commit -m "new branch commit with incremental parallel jobs"
   git push origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
-  sleep 12
+  sleep 15
   echo $b >> ~/JaaS/iteration-output-5cf-300MLF-foo-foo-0.txt
   b=$(($b+1))
 
