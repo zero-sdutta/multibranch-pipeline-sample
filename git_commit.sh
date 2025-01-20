fd18882258bc8e8a48e1c694235746129812c1c3
shuvam dutta <sdutta@cloudbees.com>
2025-01-20 16:11:50 +0530

    commit rate 6

--- git_commit.sh
+++ git_commit.sh
@@ -31,8 +31,8 @@ do
   git commit -m "new branch commit with incremental parallel jobs"
   git push origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
-  sleep 12
-  echo $b >> ~/JaaS/iteration-output-5cf-300MLF-foo-foo-sample.txt
+  sleep 10
+  echo $b >> ~/JaaS/iteration-output-5cf-300MLF-foo-foo-sample1.txt
   b=$(($b+1))
 
 done    
