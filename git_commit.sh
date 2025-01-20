ce3d64b6c8766060c826d0995bb0df6776d457bc
shuvam dutta <sdutta@cloudbees.com>
2025-01-20 13:16:48 +0530

    change commit rate and duration

--- git_commit.sh
+++ git_commit.sh
@@ -31,8 +31,8 @@ do
   git commit -m "new branch commit with incremental parallel jobs"
   git push origin perf-test-$b
 # commit frequency is calculated based on the time interval i.e. sleep
-  sleep 30
-  echo $b >> ~/JaaS/iteration-output-5cf-300MLF-foo-foo-000.txt
+  sleep 12
+  echo $b >> ~/JaaS/iteration-output-5cf-300MLF-foo-foo-0000.txt
   b=$(($b+1))
 
 done    
